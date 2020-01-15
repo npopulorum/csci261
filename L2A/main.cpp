a005dc72515d03b67cf30862c6db052d0dcddbc7
Gregory Manley <gregory.f.manley@gmail.com>
2020-01-15 09:36:56 -0700

    Update main.cpp

--- L2A/main.cpp
+++ L2A/main.cpp
@@ -1,10 +1,22 @@
 /*
+ * main.cpp
+ * L2A
  *
+ * Gregory Manley
+ * Nathan Populorum
  *
+ * Copyright © 2020 Gregory Manley and Nathan Populorum. All rights Reserved.
  *
- *
- *
- *
- *
- *
+ * L2A Pair programming introduction
  */
+
+#include <iostream>
+#include <cmath>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+  return 0;
+}
