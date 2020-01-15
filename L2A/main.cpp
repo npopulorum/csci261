0a361be1454d97f8b6ad61eeeaee3414a0411a44
Gregory Manley <gregory.f.manley@gmail.com>
2020-01-15 10:58:44 -0700

    Update main.cpp

--- L2A/main.cpp
+++ L2A/main.cpp
@@ -38,12 +38,12 @@ int main() {
   
   double p1XTemp, p1YTemp, p2XTemp, p2YTemp;
   
-  cout << Your first X value: "; cin >> p1XTemp;
-  cout << Your first Y value: "; cin >> p1YTemp;
-  cout << Your second X value: "; cin >> p2XTemp;
-  cout << Your second Y value: "; cin >> p2YTemp;
+  cout << "Your first X value: "; cin >> p1XTemp;
+  cout << "Your first Y value: "; cin >> p1YTemp;
+  cout << "Your second X value: "; cin >> p2XTemp;
+  cout << "Your second Y value: "; cin >> p2YTemp;
           
-  distanceForm()
+  distanceForm(p1XTemp, p2XTemp, p1YTemp, p2YTemp)
   
   return 0;
 }
